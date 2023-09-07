ec091cc53d4f624a7669c34f16b0a8f7aa1072e0
IamTheRafsan <140197678+IamTheRafsan@users.noreply.github.com>
2023-09-07 11:30:31 +0600

    Add files via upload

---
+++ update.php
@@ -0,0 +1,28 @@
+<?php
+
+$con = mysqli_connect("localhost","id21054149_servvvv","Evan12345!","id21054149_servvvv");
+
+
+$name = $_GET['n'];
+$email = $_GET['e'];
+$password = $_GET['p'];
+$district = $_GET['di'];
+$thana = $_GET['th'];
+$service = $_GET['s'];
+$category = $_GET['c'];
+$mobile = $_GET['m'];
+$description = $_GET['d'];
+
+if(mysqli_connect_errno())
+    echo "could not connect".mysqli_connect_error();
+else echo "Connected Successfully";
+
+
+$sql = "UPDATE user_table SET district='$district', thana='$thana', service='$service', category='$category', mobile='$mobile', description='$description' WHERE email='$email' AND password='$password'";
+
+$result = mysqli_query($con,$sql);
+
+if($result) echo "Data updated succussfully";
+else echo "Data was not updated";
+
+?>
