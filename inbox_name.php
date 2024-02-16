ca7285125c2107884c6f0270b840aba10785edbf
Mohammad Rafsan Yeasir <140197678+IamTheRafsan@users.noreply.github.com>
2024-02-17 00:13:53 +0600

    Create inbox_name.php

---
+++ inbox_name.php
@@ -0,0 +1,42 @@
+<?php
+
+$con = mysqli_connect("localhost","id21054149_servvvv","Evan12345!","id21054149_servvvv",);
+
+$email = $_GET['e'];
+
+$sql = "SELECT * FROM `user_table` WHERE `email` LIKE '$email'";
+$result = mysqli_query($con,$sql);
+
+$data = array();
+
+foreach($result as $item){
+    $name = $item['name'];
+    $email = $item['email'];
+    $password = $item['password'];
+    $district = $item['district'];
+    $thana = $item['thana'];
+    $service = $item['service'];
+    $category = $item['category'];
+    $mobile = $item['mobile'];
+    $description = $item['description'];
+    
+    
+    $userinfo['name'] = $name;
+    $userinfo['email'] = $email; 
+    $userinfo['password'] = $password;
+    $userinfo['district'] = $district;
+    $userinfo['thana'] = $thana;
+    $userinfo['service'] = $service;
+    $userinfo['category'] = $category;
+    $userinfo['mobile'] = $mobile;
+    $userinfo['description'] = $description;
+    
+    
+    array_push($data, $userinfo);
+    
+}
+
+
+echo json_encode($data);
+
+?>
